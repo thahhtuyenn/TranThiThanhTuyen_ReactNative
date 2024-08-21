31d322ca54fd8e86c46db893979839d66570168e
H82M19\Student <Student>
2024-08-21 10:51:43 +0700

    tuan 1 js --> tinh tien tiep dua theo tien tren hoa don + hinh anh ket qua

--- ReactNative_Lab01/Challenge04.js
+++ ReactNative_Lab01/Challenge04.js
@@ -1,5 +1,13 @@
-let price = 275;
-let tip = (price >= 50 && price <= 300) ? (price * 0.15) : (price * 0.2)
-let totalPrice = price + tip
 
-console.log(`The bill was ${price}, the tip was ${tip}, and the total value ${totalPrice}`);
+
+
+let tip = (price) => {
+   return (price >= 50 && price <= 300) ? (price * 0.15) : (price * 0.2)
+}
+
+let price = 275;
+let tips = tip(price)
+
+let totalPrice = price + tips
+
+console.log(`The bill was ${price}, the tip was ${tips}, and the total value ${totalPrice}`);
